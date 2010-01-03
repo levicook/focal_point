8e28c0abcc8b33fb2f5b17ac9e34a8f3c63a8db8
Levi Cook <levicook@gmail.com>
2010-01-02 20:41:25 -0600

    Initial commit to focal_point.

---
+++ test/focal_point_test.rb
@@ -0,0 +1,11 @@
+require 'teststrap'
+
+context "focal_point" do
+  setup do
+    false
+  end
+
+  asserts "i'm a failure :(" do
+    topic
+  end
+end
---
+++ test/teststrap.rb
@@ -0,0 +1,3 @@
+require 'rubygems'
+require 'riot'
+require 'focal_point'
