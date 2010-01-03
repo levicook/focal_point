878d8d30fc187fab1f239ecc8230de1ca8b022e6
Levi Cook <levicook@gmail.com>
2010-01-03 11:11:23 -0600

    Extracting an example; minor cleanup

---
+++ examples/main.rb
@@ -0,0 +1,44 @@
+module Quux
+  class Foo
+
+    attr_accessor :bin
+
+    def self.bar
+      5.times { sleep(1) }
+      return :bar
+    end
+
+    def bar(&block)
+      return block.call
+    end
+
+  end
+end
+
+
+focal_point('Quux::Foo.bar')
+focal_point('Quux::Foo.bar')
+
+focal_point('Quux::Foo#bar')
+focal_points('Quux::Foo#bin', 'Quux::Foo#bin=')
+
+
+fail unless Quux::Foo.bar == :bar
+
+
+fail unless Quux::Foo.new.bar do
+  5.times { sleep(1) }
+  :bar
+end == :bar
+
+
+5.times do 
+  foo = Quux::Foo.new
+  r = rand(1000)
+  foo.bin = r
+  fail unless foo.bin == r
+end
+
+at_exit do
+  FocalPoint.print_timers
+end
