f39e5b519480641f4d3af030bfc34878eda4113e
Levi Cook <levicook@gmail.com>
2010-01-03 10:39:38 -0600

    no more source strings + eval; using define_method, undef_method et al

--- lib/focal_point.rb
+++ lib/focal_point.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'hitimes'
-require 'pp'
 
 require File.expand_path(File.dirname(__FILE__)) + "/multiton"
 
@@ -9,50 +8,40 @@ class FocalPoint
 
   attr_accessor :timer
 
-  def self.[] target
-    new(target)
-  end
-
   def initialize(target)
     @target = String(target)
     @timer = Hitimes::TimedMetric.new(@target)
 
     if @target =~ /^([^#.]+)(#|\.)(.*)$/
-      @method, @scope = case $2
-                        when '#' ## we're instrumenting an instance method
-                          [$3, eval($1)]
-                        when '.' ## we're instrumenting a module method
-                          [$3, (class << eval($1); self; end)]
-                        end
+      @method_name = $3
+      @scope = case $2
+               when '#' ## we're instrumenting an instance method
+                 eval($1)
+               when '.' ## we're instrumenting a module method
+                 (class << eval($1); self; end)
+                end
     else
       $stdout.puts "FocalPoint::Error: Not sure how to instrument #{@target}"
     end
 
-    unless @scope.instance_methods.include?(@method) && false
-      # did you mean?? 
+    # make timer and unbound available to the lambda 
+    timer = @timer
+    unbound = @scope.instance_method(@method_name)
+    @scope.send(:undef_method , @method_name)
+    @scope.send(:define_method, @method_name) do |*args, &block|
+      lambda {
+        bound = unbound.bind(self)
+        timer.measure { bound.call(*args, &block) }
+      }.call
     end
-
-    @scope.module_eval(source)
   end
-
-  def source 
-    <<-CODE
-      alias :real_#{@method} :#{@method}
-      def #{@method}(*args, &block)
-        FocalPoint[#{@target.inspect}].timer.measure do
-          real_#{@method}(*args, &block)
-        end
-      end
-    CODE
-  end
-
 
   def self.print_timers
     timers = []
     ObjectSpace.each_object(FocalPoint) { |fp| timers << fp.timer }
     timers.compact.sort_by(&:sum).each do |timer|
       puts '-'*80
-      pp timer.to_hash
+      timer.to_hash.each { |k,v| puts "#{k}: #{v}" }
       puts
     end
   end
@@ -64,9 +53,8 @@ end
 #  focal_point("ClassName#instance_method", ...)
 #  focal_point("ClassName.class_method", ...)
 #
-#
 def focal_point(*targets)
-  targets.each { |t| FocalPoint[t] }
+  targets.each { |t| FocalPoint.new(t) }
 end
 alias :focal_points :focal_point
 
@@ -78,17 +66,32 @@ end
 if $0 == __FILE__
   module Quux
     class Foo
+      attr_accessor :bin
       def self.bar
         5.times { sleep(1) }
+        return :bar
       end
       def bar(&block)
-        block.call
+        return block.call
       end
     end
   end
-  focal_points('Quux::Foo.bar', 'Quux::Foo#bar')
-  Quux::Foo.bar
-  Quux::Foo.new.bar do
+  focal_point('Quux::Foo.bar')
+  focal_point('Quux::Foo.bar')
+  focal_point('Quux::Foo#bar')
+  focal_points('Quux::Foo#bin', 'Quux::Foo#bin=')
+
+  fail unless Quux::Foo.bar == :bar
+
+  fail unless Quux::Foo.new.bar do
     5.times { sleep(1) }
+    :bar
+  end == :bar
+
+  5.times do 
+    foo = Quux::Foo.new
+    r = rand(1000)
+    foo.bin = r
+    fail unless foo.bin == r
   end
 end
